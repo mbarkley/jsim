c34f66fc4cf5f1479775ff771409654b819ac4f6
Max Barkley <max.barkley@gmail.com>
2019-12-31 21:23:24 -0500

    Minor API refactoring

--- src/main/java/ca/mbarkley/jsim/Calculator.java
+++ src/main/java/ca/mbarkley/jsim/Calculator.java
@@ -4,31 +4,27 @@ import ca.mbarkley.jsim.model.Question;
 import ca.mbarkley.jsim.prob.Event;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 
-import java.math.BigDecimal;
-import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static ca.mbarkley.jsim.prob.Event.productOfIndependent;
-import static java.util.stream.Collectors.toList;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 @RequiredArgsConstructor
 @Builder
 public class Calculator {
     private final int scale;
 
-    public double calculateProbability(Question question) {
+    public Map<Boolean, Event<Boolean>> calculateResult(Question question) {
         final Stream<Event<Integer>> left = question.getLeft().events(scale);
         final Stream<Event<Integer>> right = question.getRight().events(scale);
-        final List<Event<Boolean>> results = productOfIndependent(left,
-                                                                  right,
-                                                                  (l, r) -> question.getComparator().evaluate(l, r)).collect(toList());
 
-        return results.stream()
-                      .filter(Event::getValue)
-                      .map(Event::getProbability)
-                      .findFirst()
-                      .orElse(0.0)
-                      .doubleValue();
+        return productOfIndependent(left,
+                                    right,
+                                    (l, r) -> question.getComparator().evaluate(l, r))
+                .collect(toMap(Event::getValue, identity()));
     }
 }
--- src/test/java/ca/mbarkley/jsim/CalculatorTest.java
+++ src/test/java/ca/mbarkley/jsim/CalculatorTest.java
@@ -1,8 +1,11 @@
 package ca.mbarkley.jsim;
 
 import ca.mbarkley.jsim.model.Question;
+import ca.mbarkley.jsim.prob.Event;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Map;
 
 public class CalculatorTest {
     Calculator calculator = new Calculator(1000);
@@ -12,53 +15,62 @@ public class CalculatorTest {
     public void rollLessThanConstant() {
         final Question question = parser.parseQuestion("d6 < 4");
 
-        final double result = calculator.calculateProbability(question);
+        final Map<Boolean, Event<Boolean>> result = calculator.calculateResult(question);
 
-        Assert.assertEquals(0.5, result, 0.0001);
+        Assert.assertEquals(0.5, result.get(true).getProbability(), 0.0001);
     }
 
     @Test
     public void complexRollGreaterThanConstant() {
         final Question question = parser.parseQuestion("2d6 + 1 > 6");
 
-        final double result = calculator.calculateProbability(question);
+        final Map<Boolean, Event<Boolean>> result = calculator.calculateResult(question);
 
-        Assert.assertEquals(0.722, result, 0.001);
+        Assert.assertEquals(0.722, result.get(true).getProbability(), 0.001);
     }
 
     @Test
     public void complexRollLessThanComplexRoll() {
         final Question question = parser.parseQuestion("2d6 + 1 < 2d8 - 4");
 
-        final double result = calculator.calculateProbability(question);
+        final Map<Boolean, Event<Boolean>> result = calculator.calculateResult(question);
 
-        Assert.assertEquals(0.20095, result, 0.00001);
+        Assert.assertEquals(0.20095, result.get(true).getProbability(), 0.00001);
     }
 
     @Test
     public void constantLessThanConstant() {
         final Question question = parser.parseQuestion("1 < 2");
 
-        final double result = calculator.calculateProbability(question);
+        final Map<Boolean, Event<Boolean>> result = calculator.calculateResult(question);
 
-        Assert.assertEquals(1.0, result, 0.00001);
+        Assert.assertEquals(1.0, result.get(true).getProbability(), 0.00001);
     }
 
     @Test
     public void bigAdditionQuestion() {
         final Question question = parser.parseQuestion("6d20 + 14d20 > 200");
 
-        final double result = calculator.calculateProbability(question);
+        final Map<Boolean, Event<Boolean>> result = calculator.calculateResult(question);
 
-        Assert.assertEquals(0.643, result, 0.001);
+        Assert.assertEquals(0.643, result.get(true).getProbability(), 0.001);
     }
 
     @Test
     public void bigMultiRollQuestion() {
         final Question question = parser.parseQuestion("20d20 > 200");
 
-        final double result = calculator.calculateProbability(question);
+        final Map<Boolean, Event<Boolean>> result = calculator.calculateResult(question);
 
-        Assert.assertEquals(0.643, result, 0.001);
+        Assert.assertEquals(0.643, result.get(true).getProbability(), 0.001);
+    }
+
+    @Test
+    public void reallyBigMultiRollQuestion() {
+        final Question question = parser.parseQuestion("100d20 > 1000");
+
+        final Map<Boolean, Event<Boolean>> result = calculator.calculateResult(question);
+
+        Assert.assertEquals(0.804, result.get(true).getProbability(), 0.001);
     }
 }
